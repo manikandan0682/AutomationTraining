7a2c74398aa8d1d5b6fb503d2548eeaeb8a71fc3
manikandan0682 <manikandan.nagarajan@yahoo.co.in>
2025-03-04 18:32:05 +0000

    lab2

--- terraform/tf-lab2/main.tf
+++ terraform/tf-lab2/main.tf
@@ -10,11 +10,10 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_instance" "lab1-tf-example" {
+resource "aws_instance" "lab2-tf-example" {
   ami           = "ami-0d5ae304a0b933620"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Lab1-TF-example"
-  }
+  Name = var.instance_name }
 }

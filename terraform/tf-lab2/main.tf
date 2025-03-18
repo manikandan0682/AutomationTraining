f0785cf7f43bc4a5e31085fcd2c4fcff511ee024
manikandan0682 <manikandan.nagarajan@yahoo.co.in>
2025-03-18 13:01:30 -0700

    variables

--- terraform/tf-lab2/main.tf
+++ terraform/tf-lab2/main.tf
@@ -1,19 +1,28 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-west-1"
 }
 
 resource "aws_instance" "lab2-tf-example" {
-  ami           = "ami-0d5ae304a0b933620"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
   Name = var.instance_name }
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
--- terraform/tf-lab2/variables.tf
+++ terraform/tf-lab2/variables.tf
@@ -3,3 +3,12 @@ variable "instance_name" {
   type        = string
   default     = "Lab2-TF-example"
 }
+
+/* variable "image_id"{
+  type=string
+  description = "The id of the machine image"
+  validation {
+    condition = len(image_id)>4 && substr(var.image_id, 0, 4) == "ami-"
+    error_message = "should start with ami_"
+  }
+} */
